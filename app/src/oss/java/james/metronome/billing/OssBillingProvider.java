9eb5506b1e518af828ac86acdf2cd27b8dd5bd04
James Fenn <me@jfenn.me>
2020-05-21 12:02:28 -0400

    update dependencies, etc

--- app/src/oss/java/james/metronome/billing/OssBillingProvider.java
+++ app/src/oss/java/james/metronome/billing/OssBillingProvider.java
@@ -9,11 +9,13 @@ import android.net.Uri;
 import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.CompoundButton;
 import android.widget.ImageView;
 
 import com.afollestad.materialdialogs.MaterialDialog;
 import com.bumptech.glide.Glide;
 
+import androidx.appcompat.widget.AppCompatCheckBox;
 import androidx.core.content.ContextCompat;
 import james.metronome.Metronome;
 import james.metronome.R;
@@ -60,6 +62,13 @@ public class OssBillingProvider implements BillingInterface {
             View view = LayoutInflater.from(activity).inflate(R.layout.dialog_premium, null);
             Glide.with(activity).load("https://jfenn.me/images/headers/metronomePremium.png").into((ImageView) view.findViewById(R.id.image));
 
+            AppCompatCheckBox checkBox = view.findViewById(R.id.hideCheckBox);
+            checkBox.setVisibility(View.VISIBLE);
+            checkBox.setOnCheckedChangeListener((buttonView, isChecked) -> {
+                SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(activity);
+                prefs.edit().putBoolean(KEY_DONATE, isChecked).apply();
+            });
+
             new MaterialDialog.Builder(activity)
                     .customView(view, false)
                     .backgroundColor(Color.WHITE)
@@ -80,8 +89,6 @@ public class OssBillingProvider implements BillingInterface {
     @Override
     public void buyPremium(Activity activity) {
         activity.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(URL_DONATE)));
-        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(activity);
-        prefs.edit().putBoolean(KEY_DONATE, true).apply();
     }
 
     @Override
